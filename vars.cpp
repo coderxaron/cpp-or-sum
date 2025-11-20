fa2866fa8362d4f67bc1bd1d3c2254d0476ed3d0
coderxaron <schnitzelwolke@gmail.com>
2025-11-20 14:52:55 +0100

    Create vars.cpp

---
+++ vars.cpp
@@ -0,0 +1,10 @@
+// lwk chat this kinda ez
+#include <iostream>
+using namespace std;
+
+int main() {
+  int apples = 10;
+  double price = 2.5;
+
+  cout << "You Pay:" << apples * price << " ." << endl;
+}
