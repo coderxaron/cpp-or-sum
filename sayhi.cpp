6c87f8e58ddb6180edbeda084ee421c02fc39243
coderxaron <schnitzelwolke@gmail.com>
2025-11-20 15:01:42 +0100

    Create sayhi.cpp

---
+++ sayhi.cpp
@@ -0,0 +1,12 @@
+// says hi to you ig
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+  string name;
+  cout << "Enter your Name:" << endl;
+  cin >> name;
+  cout << "Hello, " << name << "!" << endl;
+}
